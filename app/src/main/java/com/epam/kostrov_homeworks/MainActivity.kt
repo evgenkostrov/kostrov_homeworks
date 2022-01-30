15f529da125e2cb276b0f31be06fb20aa3c4122d
evgenkostrov <evgenkostrov@gmail.com>
2022-01-30 22:09:26 +0300

    begin InternalStorageRepository.kt

--- app/src/main/java/com/epam/kostrov_homeworks/MainActivity.kt
+++ app/src/main/java/com/epam/kostrov_homeworks/MainActivity.kt
@@ -3,11 +3,14 @@ package com.epam.kostrov_homeworks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.epam.kostrov_homeworks.databinding.ActivityMainBinding
+import com.epam.kostrov_homeworks.repository.InternalStorageRepository
 import com.epam.kostrov_homeworks.repository.SharedPreferenceRepository
 import com.epam.kostrov_homeworks.repository.TextTrain
 
 class MainActivity : AppCompatActivity() {
     lateinit var sharedPreferenceRepository: SharedPreferenceRepository
+    lateinit var internalStorageRepository: InternalStorageRepository
+
     lateinit var binding: ActivityMainBinding
 
 
@@ -17,13 +20,22 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         sharedPreferenceRepository= SharedPreferenceRepository(this)
-        binding.button.setOnClickListener { sharedPreferenceRepository.set(TextTrain(binding.textInput.text.toString()))
+        internalStorageRepository= InternalStorageRepository(this)
+
+//        binding.button.setOnClickListener { sharedPreferenceRepository.set(TextTrain(binding.textInput.text.toString()))
+//            binding.button2.setOnClickListener { binding.textOutput.text=
+//                sharedPreferenceRepository.get()?.txt ?:"empty"
+//            }
+//        }
+
+        binding.button.setOnClickListener { internalStorageRepository.set(TextTrain(binding.textInput.text.toString()))
             binding.button2.setOnClickListener { binding.textOutput.text=
-                sharedPreferenceRepository.get()?.txt ?:"empty"
+                internalStorageRepository.get().txt
             }
-
-
         }
+
+
+
 
 
     }
---
+++ app/src/main/java/com/epam/kostrov_homeworks/repository/InternalStorageRepository.kt
@@ -0,0 +1,28 @@
+package com.epam.kostrov_homeworks.repository
+
+import android.content.Context
+
+class InternalStorageRepository (private val context: Context) :Repository{
+
+
+
+    private val fileName = "TextTrain.txt"
+
+
+    override fun get(): TextTrain {
+
+       return context.openFileInput(fileName).bufferedReader().useLines { lines->
+           lines.fold(""){some,text->"$some\n$text"}
+       }.let { TextTrain(it) }
+
+    }
+
+    override fun set(textTrain: TextTrain) {
+
+        val fileContents = textTrain.txt
+        context.openFileOutput(fileName,Context.MODE_PRIVATE).use {
+            it.write(fileContents.toByteArray())
+        }
+
+    }
+}
